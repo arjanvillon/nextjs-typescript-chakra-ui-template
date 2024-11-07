6a20318b1a008f5856ea4c56a37cb31ce2a8364b
arjanvillon <heaven.arjan@gmail.com>
2024-11-07 23:50:57 +0800

    fix: optimize nextjs with new configurations

--- next.config.js
+++ next.config.js
@@ -3,13 +3,37 @@
 
 module.exports = {
   reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    optimizePackageImports: ['@chakra-ui/react'],
+    turbo: {
+      rules: {
+        '*.svg': {
+          loaders: ['@svgr/webpack'],
+          as: '*.js',
+        },
+      },
+    },
+  },
   webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
+    const fileLoaderRule = config.module.rules.find((rule) =>
+      rule.test?.test?.('.svg')
+    )
+
+    config.module.rules.push(
+      {
+        ...fileLoaderRule,
+        test: /\.svg$/i,
+        resourceQuery: /url/,
+      },
+      {
+        test: /\.svg$/i,
+        issuer: fileLoaderRule.issuer,
+        resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+        use: ['@svgr/webpack'],
+      }
+    )
+
+    fileLoaderRule.exclude = /\.svg$/i
 
     return config
   },
